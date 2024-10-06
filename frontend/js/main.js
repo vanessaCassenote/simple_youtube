188db8bebf4347c3d9394ccfb2f8aa1166346122
vanessaCassenote <vanessacassenote.92@gmail.com>
2024-10-06 17:33:03 -0300

    feat: watch videos

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -17,42 +17,54 @@ function addLogin(){
 }
 
 function login(){
-    addLogout()
-    
-    // fetch("http://127.0.0.1:5000/login", 
+    // var endUpload = fetch("http://127.0.0.1:5000/login", 
     //     {
     //         method: 'GET',
-    //         headers: {
-    //             'Content-type': 'application/json',
-    //             'Accept': 'application/json'
-    //         },
-    //     }).then(res=>{
-    //             if(res.ok){
-    //                 return res.json()
-    //             }else{
-    //                 alert("something is wrong")
-    //             }
-    //         }).then(jsonResponse=>{
-    //                 window.location.href =  jsonResponse["url"]
-    //                 console.log(jsonResponse)
-    //         }).catch((err) => console.error(err));
-            
+    //         headers: { 'Content-Type': 'application/json' }
+    //     })
+    //     .then(response => response.json())
+    //     .then(jsonResponse=> {
+    //         window.location.href = jsonResponse['response']
+    //         console.log(jsonResponse)
+    //     })
+    //     .catch((err) => console.error(err));
+    
+    addLogout()
 }
 
 
 document.getElementById("inputGroupFile02").addEventListener('change', 
     async function(event){
+        const chunksText = document.getElementById("video-information")
+        const uploadBody = document.getElementById("uploading")
+        const progressBar = document.createElement("div")
+        const progressBarInner = document.createElement("div")
+
+        progressBarInner.setAttribute("class", "progress-bar bg-success")
+        progressBarInner.setAttribute("style", "width: 6.25%")
+        progressBar.setAttribute("class","progress")
+        progressBar.setAttribute("role", "progressbar")
+        progressBar.setAttribute("aria-label", "Success example")
+        progressBar.setAttribute("aria-valuenow", "25")
+        progressBar.setAttribute("aria-valuemin", "0")
+        progressBar.setAttribute("aria-valuemax", "100")
+        progressBar.appendChild(progressBarInner)
+
         var file = event.target.files[0];
         const chunkSize = 1024*1024*10; //10MB 
 
         var numberOfChunks = Math.ceil(file.size/chunkSize);
         const upload_id = file.name+numberOfChunks
         const uploadPromises =[]
-        document.getElementById("video-information").innerHTML = "There will be " + numberOfChunks + " chunks uploaded."
         var start=0; 
         var chunkEnd = Math.min.apply(null,[start + chunkSize , file.size])
 
         console.log("Start:")
+        
+        perc = parseFloat(Math.ceil(100/numberOfChunks));
+        percWidth = perc
+        chunksText.innerHTML = "There will be " + numberOfChunks + " chunks uploaded "
+        uploadBody.appendChild(progressBar)
 
         for (let i=1; i<=numberOfChunks;i++){
             const blobData = file.slice(start, start+chunkSize, contentType="video/mp4");
@@ -86,11 +98,18 @@ document.getElementById("inputGroupFile02").addEventListener('change',
 
             var response = fetch("http://127.0.0.1:5000/upload_parts", requestOptions)
                     .then(response => response.json())
-                    .then(jsonResponse=> console.log(jsonResponse))
+                    .then(jsonResponse=> {
+                        percWidth = Math.min.apply(null,[percWidth,100])
+                        progressBarInner.style.width = percWidth + "%";
+                        progressBarInner.innerHTML = percWidth + "%";
+                        percWidth += perc
+                        console.log(jsonResponse)
+                    })
                     .catch((err) => console.error(err));
-
+            
             console.log("chunk: "+i)
             uploadPromises.push(response)
+
         }
         var endUpload = fetch("http://127.0.0.1:5000/upload_end", 
         {
@@ -105,6 +124,13 @@ document.getElementById("inputGroupFile02").addEventListener('change',
         uploadPromises.push(endUpload)
 
         await Promise.all(uploadPromises)
+
+
+        let enterEvent = new KeyboardEvent("F5", {
+            code: 116
+        });
+          
+        inputElement.dispatchEvent(enterEvent);
         
 });
 
@@ -133,7 +159,7 @@ function addLogout(){
 }
 
 
-function addItem(title,thumbnail,id){
+function addItem(title,thumbnail,id,url){
     
     let mainArea = document.getElementById("main-area")
     var novoItem = document.createElement('div'); 
@@ -142,17 +168,17 @@ function addItem(title,thumbnail,id){
         <div class="card" style="width: 18rem;">
             <img class="card-img-top" src="${thumbnail}" alt="Card image cap"> 
             <div class="card-body">
-                <a id="id_${id}" href="#" class="btn btn-primary d-grid gap-2 justify-content-md-end">
+                <a id="id_${id}" onclick="watchVideo('${url}')" href="#" class="btn btn-primary d-grid gap-2 justify-content-md-end">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-play-btn" viewBox="0 0 16 16">
                         <path d="M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z"/>
                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
                     </svg>
                 </a>
-                <h5 class="card-title h5-spaces">${title}</h5>
+                <p class="card-title fw-semibold">${title}</p>
             </div>
         </div>
     </div>  `
-    mainArea.appendChild(novoItem)
+    mainArea.prepend(novoItem)
 }
 
 function addVideos(jsonResponse){
@@ -161,11 +187,54 @@ function addVideos(jsonResponse){
         let title = jsonResponse[resp]["title"]
         let thumbnail = "download.svg" //jsonResponse[resp]["thumbnail"]
         let id = jsonResponse[resp]["id"]
+        let url = jsonResponse[resp]["url"]
         
-        addItem(title, thumbnail, id)
+        addItem(title, thumbnail, id, url)
     }
 
 }
+
+function index(){
+    let main = document.getElementById("main-area")
+    let main_watch = document.getElementById("main-area-watch")
+
+    let btn_main_watch = document.getElementById("btn_main_watch")
+    var video = document.getElementById("video")
+    var source = document.getElementById("source")
+
+    main.style.display = "block";
+
+    main_watch.removeChild(btn_main_watch)
+    main_watch.removeChild(video)
+}
+
+function watchVideo(url){
+
+    let main = document.getElementById("main-area")
+    let main_watch = document.getElementById("main-area-watch")
+    let btn_main_watch = document.createElement("button")
+    var video = document.createElement("video")
+    var source = document.createElement("source")
+    main.style.display = "none";
+
+    btn_main_watch.textContent = "Back"
+    btn_main_watch.setAttribute("onclick", "index()")
+    btn_main_watch.id = "btn_main_watch"
+    btn_main_watch.className = "btn btn-outline-success me-2"
+
+    source.src = url
+    source.type = "video/mp4"
+    source.id = "source"
+    video.setAttribute("controls","controls") 
+    video.appendChild(source);
+    video.id = "video"
+    video.setAttribute("autoplay", "autoplay")
+    
+    main_watch.appendChild(btn_main_watch)
+    main_watch.appendChild(video)
+
+}
+
 
 fetch("http://127.0.0.1:5000/", 
     {
